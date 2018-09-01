f56fc0ce0d96460cfb88112335b010d3dfe295d0
snehasish <karak.snehasish@gmail.com>
2018-09-01 23:19:53 +0530

    added code

---
+++ GitTest/src/TestClass.java
@@ -0,0 +1,16 @@
+
+public class TestClass {
+	
+	String []names={"name1","name2","name3"};
+	
+	public void dispalyNames(){
+		for(int i=0;i<names.length;i++){
+			System.out.println("names to be displayed");
+			System.out.println("names is"+names[i]);
+		}
+	}
+	
+	
+	
+
+}
