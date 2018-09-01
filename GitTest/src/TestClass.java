8bcc52cb9045c243286afe3e1f04177f33aeb9fd
snehasish <karak.snehasish@gmail.com>
2018-09-01 23:44:48 +0530

    loop modified

--- GitTest/src/TestClass.java
+++ GitTest/src/TestClass.java
@@ -4,9 +4,10 @@ public class TestClass {
 	String []names={"name1","name2","name3"};
 	
 	public void dispalyNames(){
-		for(int i=0;i<names.length;i++){
+		//Modified to Advanced for loop
+		for(String nm:names){
 			System.out.println("names to be displayed");
-			System.out.println("names is"+names[i]);
+			System.out.println("names is"+nm);
 		}
 	}
 	
