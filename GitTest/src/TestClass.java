2ead456c3feaa8e1e2a503982718bb5d4e5c9887
snehasish <karak1.snehasish@gmail.com>
2018-09-01 23:50:22 +0530

    added two more names and extra sysout

--- GitTest/src/TestClass.java
+++ GitTest/src/TestClass.java
@@ -1,12 +1,14 @@
 
 public class TestClass {
 	
-	String []names={"name1","name2","name3"};
+	String []names={"name1","name2","name3","name4","name5"};
 	
 	public void dispalyNames(){
 		for(int i=0;i<names.length;i++){
 			System.out.println("names to be displayed");
 			System.out.println("names is"+names[i]);
+			System.out.println("names dispaled");
+	
 		}
 	}
 	
