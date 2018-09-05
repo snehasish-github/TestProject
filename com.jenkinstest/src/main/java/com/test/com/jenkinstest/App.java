63b70a4780ee3c89fa38c60c72164200f92cb961
snehasish <karak.snehasish@gmail.com>
2018-09-05 23:29:48 +0530

    initial commit

---
+++ com.jenkinstest/src/main/java/com/test/com/jenkinstest/App.java
@@ -0,0 +1,13 @@
+package com.test.com.jenkinstest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
